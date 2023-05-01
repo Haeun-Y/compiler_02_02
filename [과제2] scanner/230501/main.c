0c965eab83e5553eef3e24bcbca85d1b176fec13
Haeun-Y <70390323+Haeun-Y@users.noreply.github.com>
2023-05-01 20:44:43 +0900

    [230501] 출력 수정

--- [과제2] scanner/230501/main.c
+++ [과제2] scanner/230501/main.c
@@ -8,9 +8,9 @@
 void PrintHeading()
 {
 	printf("\n\n");
-	printf("  -----------      ------------       ------------       ------------ \n");
-	printf("  line Number       Token type            Token            ST-index   \n");
-	printf("  -----------      ------------       ------------       ------------ \n");
+	printf("  -----------        ------------         ------------         ------------ \n");
+	printf("  line Number         Token type              Token              ST-index   \n");
+	printf("  -----------        ------------         ------------         ------------ \n");
 	printf("\n");
 }
 
@@ -18,51 +18,52 @@ void PrintHeading()
 	identifier의 경우 String Table에서의 index도 출력함 */
 void printToken(enum tokentypes tn)
 {
-	if(tn == TCOMMENT) printf("%5d\t\t\t", startLineNumber);
-	else printf("%5d\t\t\t", lineNumber);
+	if(tn == TCOMMENT) printf("  %-20d", startLineNumber);
+	else printf("  %-20d", lineNumber);
+	//printf("%-20s %-20s %-20d\n", "identifier", "abc", 4);
 	switch (tn) {
-	case TCONST: printf("%-20s %s\n", "const", yytext);  break;
-	case TELSE: printf("%-20s %s\n", "else", yytext);  break;
-	case TIF: printf("%-20s %s\n", "if", yytext); break;
-	case TINT: printf("%-20s %s\n", "int", yytext); break;
-	case TRETURN: printf("%-20s %s\n", "return", yytext);  break;
-	case TVOID: printf("%-20s %s\n", "void", yytext);  break;
-	case TWHILE: printf("%-20s %s\n", "while", yytext); break;
-	case TADD: printf("%-20s %s\n", "add", yytext); break;
-	case TSUB: printf("%-20s %s\n", "subtract", yytext); break;
-	case TMUL: printf("%-20s %s\n", "multiply", yytext); break;
-	case TDIV: printf("%-20s %s\n", "divide", yytext); break;
-	case TMOD: printf("%-20s %s\n", "mod", yytext); break;
-	case TNOT: printf("%-20s %s\n", "not", yytext); break;
-	case TAND: printf("%-20s %s\n", "and", yytext);   break;
-	case TOR: printf("%-20s %s\n", "or", yytext);   break;
-	case TEQUAL: printf("%-20s %s\n", "equal", yytext); break;
-	case TNOTEQU: printf("%-20s %s\n", "not_equal", yytext);   break;
-	case TLESSE: printf("%-20s %s\n", "equal_to_or_less_than", yytext);   break;
-	case TGREATE: printf("%-20s %s\n", "equal_to_or_greater_than", yytext);   break;
-	case TLESS: printf("%-20s %s\n", "less_than", yytext);   break;
-	case TGREAT: printf("%-20s %s\n", "greater_than", yytext);   break;
-	case TINC: printf("%-20s %s\n", "increment", yytext);   break;
-	case TDEC: printf("%-20s %s\n", "decrement", yytext);   break;
-	case TASSIGN: printf("%-20s %s\n", "assign", yytext);   break;
-	case TADDASSIGN: printf("%-20s %s\n", "add_assign", yytext);   break;
-	case TSUBASSIGN: printf("%-20s %s\n", "sub_assign", yytext);   break;
-	case TMULASSIGN: printf("%-20s %s\n", "mul_assign", yytext);   break;
-	case TDIVASSIGN: printf("%-20s %s\n", "div_assign", yytext);   break;
-	case TMODASSIGN: printf("%-20s %s\n", "mod_assign", yytext);   break;
-	case TLPAREN: printf("%-20s %s\n", "left parenthesis", yytext);   break;
-	case TRPAREN: printf("%-20s %s\n", "right parenthesis", yytext);   break;
-	case TCOMMA: printf("%-20s %s\n", "comma", yytext);   break;
-	case TLBRACE: printf("%-20s %s\n", "left brace", yytext);   break;
-	case TRBRACE: printf("%-20s %s\n", "right brace", yytext);   break;
-	case TLBRACKET: printf("%-20s %s\n", "left bracket", yytext);   break;
-	case TRBRACKET: printf("%-20s %s\n", "right bracket", yytext);   break;
-	case TSEMI: printf("%-20s %s\n", "semicolon", yytext);   break;
-	case TNUMBER: printf("%-20s %s\n", "number", yytext);   break;
+	case TCONST: printf("%-20s %-20s\n", "const", yytext);  break;
+	case TELSE: printf("%-20s %-20s\n", "else", yytext);  break;
+	case TIF: printf("%-20s %-20s\n", "if", yytext); break;
+	case TINT: printf("%-20s %-20s\n", "int", yytext); break;
+	case TRETURN: printf("%-20s %-20s\n", "return", yytext);  break;
+	case TVOID: printf("%-20s %-20s\n", "void", yytext);  break;
+	case TWHILE: printf("%-20s %-20s\n", "while", yytext); break;
+	case TADD: printf("%-20s %-20s\n", "add", yytext); break;
+	case TSUB: printf("%-20s %-20s\n", "subtract", yytext); break;
+	case TMUL: printf("%-20s %-20s\n", "multiply", yytext); break;
+	case TDIV: printf("%-20s %-20s\n", "divide", yytext); break;
+	case TMOD: printf("%-20s %-20s\n", "mod", yytext); break;
+	case TNOT: printf("%-20s %-20s\n", "not", yytext); break;
+	case TAND: printf("%-20s %-20s\n", "and", yytext);   break;
+	case TOR: printf("%-20s %-20s\n", "or", yytext);   break;
+	case TEQUAL: printf("%-20s %-20s\n", "equal", yytext); break;
+	case TNOTEQU: printf("%-20s %-20s\n", "not_equal", yytext);   break;
+	case TLESSE: printf("%-20s %-20s\n", "equal_to_or_less_than", yytext);   break;
+	case TGREATE: printf("%-20s %-20s\n", "equal_to_or_greater_than", yytext);   break;
+	case TLESS: printf("%-20s %-20s\n", "less_than", yytext);   break;
+	case TGREAT: printf("%-20s %-20s\n", "greater_than", yytext);   break;
+	case TINC: printf("%-20s %-20s\n", "increment", yytext);   break;
+	case TDEC: printf("%-20s %-20s\n", "decrement", yytext);   break;
+	case TASSIGN: printf("%-20s %-20s\n", "assign", yytext);   break;
+	case TADDASSIGN: printf("%-20s %-20s\n", "add_assign", yytext);   break;
+	case TSUBASSIGN: printf("%-20s %-20s\n", "sub_assign", yytext);   break;
+	case TMULASSIGN: printf("%-20s %-20s\n", "mul_assign", yytext);   break;
+	case TDIVASSIGN: printf("%-20s %-20s\n", "div_assign", yytext);   break;
+	case TMODASSIGN: printf("%-20s %-20s\n", "mod_assign", yytext);   break;
+	case TLPAREN: printf("%-20s %-20s\n", "left parenthesis", yytext);   break;
+	case TRPAREN: printf("%-20s %-20s\n", "right parenthesis", yytext);   break;
+	case TCOMMA: printf("%-20s %-20s\n", "comma", yytext);   break;
+	case TLBRACE: printf("%-20s %-20s\n", "left brace", yytext);   break;
+	case TRBRACE: printf("%-20s %-20s\n", "right brace", yytext);   break;
+	case TLBRACKET: printf("%-20s %-20s\n", "left bracket", yytext);   break;
+	case TRBRACKET: printf("%-20s %-20s\n", "right bracket", yytext);   break;
+	case TSEMI: printf("%-20s %-20s\n", "semicolon", yytext);   break;
+	case TNUMBER: printf("%-20s %-20s\n", "number", yytext);   break;
 
-	case TIDENT: printf("%-20s %s \t\t\t%d\n", "identifier", yytext, sameid);   break;
-	case TCOMMENT: printf("%-20s %s\n", "comment", yytext);   break;
-	case TERROR: printf("%-20s %s\n", "**Error**", error_message);
+	case TIDENT: printf("%-20s %-20s %-20d\n", "identifier", yytext, sameid);   break;
+	case TCOMMENT: printf("%-20s %-20s\n", "comment", yytext);   break;
+	case TERROR: printf("%-20s %-20s\n", "**Error**", error_message);
 		if (err == overst)
 		{
 			printf("Program terminated due to overflow of ST.\n");
