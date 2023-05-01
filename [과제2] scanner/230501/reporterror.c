ae5bf55537a2459a52b0ca06bc5acdb933d4b079
Haeun-Y <70390323+Haeun-Y@users.noreply.github.com>
2023-05-01 20:26:08 +0900

    [230501] reporterror

--- [과제2] scanner/230501/reporterror.c
+++ [과제2] scanner/230501/reporterror.c
@@ -1,8 +1,14 @@
+/*
+* reporterror.c - error report
+* programmer - 권영경, 옥진주, 윤하은, 최예원
+* date - 2023/04/27
+*/
 /* error messages
 			overst :  overflow in ST. print the hashtable and abort
 			illid_digit    : illegal identifier (start with digit)
 			illid_long	: illegal identifier (too long identifier)
-			illid_illch	: illegal identifier (containing illegal characters)
+			illid_illch	: illegal characters
+			real_num : real number 
 */
 #include "tn.h"
 
@@ -18,10 +24,10 @@ void ReportError(ERRORtypes err)
 		strcpy(error_message, "too long identifier");
 		break;
 	case illch:
-		sprintf(error_message, "illegal character %s", yytext);
+		sprintf(error_message, "%s illegal character", yytext);
 		break;
 	case illid_digit:
-		strcpy(error_message, "start with digit");
+		sprintf(error_message, "%s start with digit", yytext);
 		break;
 	case real_num:
 		strcpy(error_message, "real number is not allowed");
