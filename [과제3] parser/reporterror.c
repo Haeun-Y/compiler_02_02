143990f21c28a99b7c71c8429433b4f27a78e4f2
Kwon Yeongkyeong <86418308+asaei623@users.noreply.github.com>
2023-05-30 17:02:37 +0900

    Update reporterror.c

--- [과제3] parser/reporterror.c
+++ [과제3] parser/reporterror.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "glob.h"
 
-
-/* 
+extern yytext;
+/*
 	yyerror(parser에서 발생한 에러 처리)
 
 		lineNumber : 에러가 발생한 행
@@ -23,8 +23,8 @@
 */
 yyerror(char* sb)
 {
-	cErrors++
-	printf("line %d: %s in %s \n", lineNumber, sb, yytext);
+	cErrors++;
+		printf("line %d: %s in %s \n", lineNumber, sb, yytext);
 }
 
 void ReportError(ERRORtypes err)
@@ -32,21 +32,21 @@ void ReportError(ERRORtypes err)
 	cErrors++;
 
 	switch (err) {
-		case overst:
-			strcpy(error_message, "overflow");
-			break;
-		case illid_long:
-			strcpy(error_message, "too long identifier");
-			break;
-		case illch:
-			sprintf(error_message, "%s illegal character", yytext);
-			break;
-		case illid_digit:
-			sprintf(error_message, "%s start with digit", yytext);
-			break;
-		case real_num:
-			strcpy(error_message, "real number is not allowed");
-			//실수가 들어올 경우 에러 발생. Floating Point 경우는 제외하였음.
-			break;
-		}
+	case overst:
+		strcpy(error_message, "overflow");
+		break;
+	case illid_long:
+		strcpy(error_message, "too long identifier");
+		break;
+	case illch:
+		sprintf(error_message, "%s illegal character", yytext);
+		break;
+	case illid_digit:
+		sprintf(error_message, "%s start with digit", yytext);
+		break;
+	case real_num:
+		strcpy(error_message, "real number is not allowed");
+		//실수가 들어올 경우 에러 발생. Floating Point 경우는 제외하였음.
+		break;
+	}
 }
